08c73c87ea4366cd46dd328c5ac1d23a87bcf1ae
samuelikz <samuel.nds@outlook.com>
2021-11-01 20:36:22 -0300

    Criando Metodos

--- src/index.js
+++ src/index.js
@@ -5,8 +5,24 @@ const express = require('express');
 const app = express(); 
 
 //Criar requisição
-app.get('/', (request, response) =>{
-    return response.json({message: "Hello World Ignite!"})
+app.get('/courses', (request, response) =>{
+    return response.json(["Curse 1", "Curse 2", "Curse 3","Curse 4"]);
+})
+
+app.post('/courses', (request, response) => {
+    return response.json(["Curse 1", "Curse 2", "Curse 3","Curse 4"]);
+})
+
+app.put('courses/:id', (request, response) => {
+    return response.json(["Curse 01", "Curse 2", "Curse 3","Curse 4"]);
+})
+
+app.patch('courses/:id', (request, response) => {
+    return response.json(["Curse 01", "Curse 2", "Curse 3","Curse 04"]);
+})
+
+app.delete('courses/:id', (request, response) => {
+    return response.json(["Curse 01", "Curse 2", "Curse 3"]);
 })
 
 // porta da aplicação
